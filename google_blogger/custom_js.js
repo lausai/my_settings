858823eb8ea0571d356f4703feb35245b1a97644
lausai <lausai360@gmail.com>
2019-03-21 22:29:08 +0800

    Change:     1. Auto add prettyprint to <pre> tag
        2. Modify the padding for blockquote

--- google_blogger/custom_js.js
+++ google_blogger/custom_js.js
@@ -1,13 +1,25 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.6.4/showdown.min.js">
 </script>
 <script>
-var converter = new showdown.Converter();
 
+// The > will replaced to &gt; when using innerHTML, so replace it back to >
+// for showdown to render blockquote
+function blockquote(str) {
+    return str.replace(/\n&gt;/g,'\n>') ;
+}
+
+var converter = new showdown.Converter();
 var posts = document.querySelectorAll(".post-body");
-Array.prototype.forEach.call(posts, function(el, i){
+var s = "";
+Array.prototype.forEach.call(posts, function(el, i) {
     var idx = el.innerHTML.indexOf("markdown");
-    if(idx != -1 && idx <= 1){
-        el.innerHTML = converter.makeHtml(el.innerHTML.replace("markdown",""));
+    if(idx != -1 && idx <= 1) {
+        el.innerHTML = converter.makeHtml(blockquote(el.innerHTML).replace("markdown",""));
+        var pres = el.querySelectorAll("pre");
+        for (var i = 0; i < pres.length; i++) {
+            pres[i].classList.add("prettyprint");
+            pres[i].classList.add("linenums:1");
+        }
     }
 });
 </script>
